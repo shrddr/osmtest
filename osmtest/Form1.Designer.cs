c642eff5c28a02fd0330bd78ef13f4de79d63fd0
shrddr <qwerty-zigzag@mail.ru>
2016-07-18 17:28:28 +0300

    test

--- osmtest/Form1.Designer.cs
+++ osmtest/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.XAPIButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // XAPIButton
+            // 
+            this.XAPIButton.Location = new System.Drawing.Point(525, 14);
+            this.XAPIButton.Name = "XAPIButton";
+            this.XAPIButton.Size = new System.Drawing.Size(75, 23);
+            this.XAPIButton.TabIndex = 2;
+            this.XAPIButton.Text = "XAPI";
+            this.XAPIButton.UseVisualStyleBackColor = true;
+            this.XAPIButton.Click += new System.EventHandler(this.XAPIbutton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(525, 43);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(386, 521);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(607, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(426, 542);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 577);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.XAPIButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button XAPIButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- osmtest/Form1.cs
+++ osmtest/Form1.cs
@@ -4,17 +4,105 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OsmSharp;
+using OsmSharp.Math.Geo;
+using OsmSharp.Osm;
+using OsmSharp.Osm.Xml.Streams;
+using OsmSharp.UI.Map;
+using OsmSharp.UI.Map.Layers;
+using OsmSharp.UI.Map.Styles;
+using OsmSharp.WinForms.UI;
 
 namespace osmtest
 {
     public partial class Form1 : Form
     {
+        MapControl mapControl;
+        LayerPrimitives layer;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void XAPIbutton_Click(object sender, EventArgs e)
+        {
+            DateTime n = DateTime.Now;
+
+            var query = "way[building=*][bbox=27.48017,53.93668,27.48516,53.93945]";
+            var request = WebRequest.Create("http://overpass.osm.rambler.ru/cgi/xapi_meta?" + query);
+            request.Method = "GET";
+            var requestStream = request.GetResponse().GetResponseStream();
+            var source = new XmlOsmStreamSource(requestStream);
+
+            var osmEntities = new List<OsmGeo>(source);
+            var outputCsv = new List<string[]>();
+            for (int idx = 0; idx < osmEntities.Count; idx++)
+            {
+                var node = osmEntities[idx] as Node;
+                var refId = string.Empty;
+                var name = string.Empty;
+                if (node != null)
+                {
+                    var latitude = node.Coordinate.Latitude;
+                    var longitude = node.Coordinate.Longitude;
+
+                    outputCsv.Add(new string[] { latitude.ToInvariantString(), longitude.ToInvariantString() });
+                }
+            }
+
+            foreach (var item in outputCsv)
+            {
+                textBox2.Text += item[0];
+                textBox2.Text += ' ';
+                textBox2.Text += item[1];
+                textBox2.Text += Environment.NewLine;
+            }
+
+            label2.Text = (DateTime.Now - n).ToString();
+
+            var map = new Map();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var map = new Map();
+            layer = new LayerPrimitives(new OsmSharp.Math.Geo.Projections.WebMercator());
+            layer.AddPoint(new GeoCoordinate(53.9384963, 27.483288), 1, 1);
+            layer.AddPoint(new GeoCoordinate(53.9386293, 27.4834595), 1, 2);
+            layer.AddPoint(new GeoCoordinate(53.9385067, 27.4837339), 1, 3);
+            layer.AddPoint(new GeoCoordinate(53.9383737, 27.4835625), 1, 4);
+            
+            map.AddLayer(layer);
+            layer.AddLine(new GeoCoordinate(53.9384963, 27.483288), new GeoCoordinate(53.9386293, 27.4834595), 1, 2);
+
+            mapControl = new MapControl();
+            mapControl.Location = new Point(10, 10);
+            mapControl.Size = new Size(500, 500);
+            mapControl.BackColor = Color.Pink;
+
+            mapControl.Map = map;
+            mapControl.MapCenter = new GeoCoordinate(53.9385, 27.4835);
+            mapControl.MapZoom = 18;
+            mapControl.Refresh();
+        
+            
+
+
+            Controls.Add(mapControl);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var rand = new Random();
+            var dx = (rand.NextDouble() - 0.5) / 1000;
+            var dy = (rand.NextDouble() - 0.5) / 1000;
+            layer.AddPoint(new GeoCoordinate(mapControl.MapCenter.Latitude + dx, mapControl.MapCenter.Longitude + dy), 5, 1);
         }
     }
 }
